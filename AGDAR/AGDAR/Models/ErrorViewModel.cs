c288ce742ab968e81e1691a6cef497188bc3223b
Dawid Gramacki <72979362+Karzelus@users.noreply.github.com>
2023-05-30 20:39:18 +0200

    Add files via upload

---
+++ AGDAR/AGDAR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AGDAR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
